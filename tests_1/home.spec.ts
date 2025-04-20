e651b93874696fe605fe3b2be6d0b85f6eae8ba0
esmael <esmaellucaya@gmail.com>
2025-04-21 01:58:13 +0800

    initial commit

--- tests_1/home.spec.ts
+++ tests_1/home.spec.ts
@@ -1,34 +1,50 @@
 import {test, expect } from "@playwright/test"
 
+test.describe("Home page with no auth", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto("https://practicesoftwaretesting.com/");
+    });
 
-test("Home page", async({ page }) => {
-
-    await page.goto("https://practicesoftwaretesting.com/");
+test("Validate Sign-in", async({ page }) => {
     // Ensure the sign-in butto link is present
     await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
-    // Check the title of the page
+});
+ // Check the title of the page
+test("Validate title page and items  display on site", async({ page }) => { 
     await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0");
     // Check the count of the item displayed
     const productgrid = page.locator(".col-md-9")
     await expect(productgrid.getByRole("link")).toHaveCount(9);
+});
 
-    // using non locator assertions
+// test("Validate grid count for non Assertions", async({ page }) => {
+//     // using non locator assertions
+//     //expect(await productgrid.getByRole("link").count()).toBe(9);
+// });
 
-    //expect(await productgrid.getByRole("link").count()).toBe(9);
-
-    
-
+test("Validate Searched function", async({ page }) => {
     // Searched for Thor hammer
     // create a script for searching capabilities
-
     await page.getByLabel("Search").fill("Thor");
     await page.locator('[data-test="search-submit"]').click();
+    const productgrid = page.locator(".col-md-9")
     await expect(productgrid.getByRole("link")).toHaveCount(1);
-    await expect(page.locator('[data-test="search-term"]')).toBeVisible();
+    await expect(page.locator('[data-test="search-term"]')).toBeVisible()
+});
+   
+  
+});
 
-    // using non locator assertions
+// with auth
 
-    //expect(await productgrid.getByRole("link").count()).toBe(9);
+test.describe("Home page with customer01 with auth", () => {
 
-    
-})
+    test.use({storageState: ".auth/customer01.json"});
+    test.beforeEach(async ({ page }) => {
+        await page.goto("https://practicesoftwaretesting.com/");
+    });
+
+    test("check customer01 is signed in", async ({ page }) => {
+        await expect(page.getByTestId("nav-sign-in")).not.toBeVisible();
+    })
+});
